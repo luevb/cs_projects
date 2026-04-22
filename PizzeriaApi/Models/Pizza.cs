df7d31e3c6ab8b64a9ba7f5f87b739da982b7aab
luevb <luevboris1345@gmail.com>
2026-04-22 13:38:03 +0300

    add Models, add Data

--- PizzeriaApi/Models/Pizza.cs
+++ PizzeriaApi/Models/Pizza.cs
@@ -1,6 +1,25 @@
-﻿namespace PizzeriaApi.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PizzeriaApi.Models
 {
     public class Pizza
     {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        [Display(Name = "Название пиццы")]
+        public string Name { get; set; }
+
+        [Required]
+        public int CategoryId { get; set; }
+
+        public Category? Category { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        public ICollection<PizzaSize> PizzaSizes { get; set; }
     }
 }
--- PizzeriaApi/Models/Size.cs
+++ PizzeriaApi/Models/Size.cs
@@ -1,6 +1,26 @@
-﻿namespace PizzeriaApi.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PizzeriaApi.Models
 {
     public class Size
     {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public int Diametr { get; set; }
+
+        public ICollection<PizzaSize> PizzaSizes { get; set; }
+
+        public Size() { }
+
+        public Size(string name, int diametr)
+        {
+            Name = name;
+            Diametr = diametr;
+        }
     }
 }
