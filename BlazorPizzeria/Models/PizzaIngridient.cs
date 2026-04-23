8f5bea771e1574f27751eb641c4bb9a47ad1c392
luevb <luevboris1345@gmail.com>
2026-04-23 23:40:09 +0300

    add models

---
+++ BlazorPizzeria/Models/PizzaIngridient.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorPizzeria.Models
+{
+    public class PizzaIngredient
+    {
+        public int PizzaId { get; set; }
+        public Pizza Pizza { get; set; } = null!;
+        public int IngredientId { get; set; }
+        public Ingredient Ingredient { get; set; } = null!;
+    }
+}
